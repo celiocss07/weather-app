675af93f0873f5e0f00e16c29e30044661c20493
= <celiocss07@gmail.com>
2022-07-13 19:49:12 +0100

    style: add layout structure from home page using flexbox

--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -2,4 +2,17 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   flex: 1;
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  font-size: calc(10px + 2vmin);
+
+  @media(min-width: 640px){
+    flex-direction: row;
+    justify-content: space-between;
+    
+  }
+  
 `;
