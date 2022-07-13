70e1b6ffc32dd84237931b45f5644f96e5a5c3da
= <celiocss07@gmail.com>
2022-07-13 20:13:36 +0100

    feat(main): create header component

---
+++ src/components/MainHeader/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from './style'
+
+export default function MainHeader() {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
---
+++ src/components/MainHeader/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  width: 100%;
+  padding: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
