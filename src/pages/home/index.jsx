09859dd022a003e633895e88e0277a0ca111dac6
= <celiocss07@gmail.com>
2022-07-13 19:35:47 +0100

    perf: create router structure and home page

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+function Home() {
+  return (
+  <Container>
+    Hello, wolrd!
+  </Container>
+  );
+}
+
+export default Home;
---
+++ src/routes/routes.js
@@ -0,0 +1,14 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./../pages/home";
+import React from "react";
+
+export default function MyRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
